09beb04bae140dde3c62f8203dcd4f81cbfd70bd
Akash <Akash@192.168.1.92>
2022-10-28 17:25:21 +0530

    Commit 11

--- php.crm.Vtiger.Project1/src/main/java/php/crm/Vtiger/Project1/genericUtitlity/WebDriverUtility.java
+++ php.crm.Vtiger.Project1/src/main/java/php/crm/Vtiger/Project1/genericUtitlity/WebDriverUtility.java
@@ -6,7 +6,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+/**
+ * 
+ * @author Akash
+ *
+ */
 public class WebDriverUtility {
 	
 	public void implicitWait(WebDriver driver) {
